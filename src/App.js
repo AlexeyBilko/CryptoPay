53ee521f413e057deaa8d132aa32ef7df321a0d5
Alexey Bilko <bilkoalexey67@gmail.com>
2024-05-21 15:56:48 +0300

    added basename

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import EarningsPage from './Pages/Dashboard/EarningsPage'
 
 function App() {
   return (
-      <Router>
+      <Router basename="/cryptopay">
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/login" element={<LoginPage />} />
--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://cryptopaymentgatewaybackend.azurewebsites.net';//'http://localhost:5000/api'; // Adjust according to API endpoint
+const API_URL = 'https://cryptopaymentgatewaybackend.azurewebsites.net/api';//'http://localhost:5000/api'; // Adjust according to API endpoint
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
