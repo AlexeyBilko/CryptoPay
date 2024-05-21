713da61d5f0771ced6897c81e79a272ee356f5bc
Alexey Bilko <bilkoalexey67@gmail.com>
2024-05-21 15:38:32 +0300

    Updates

---
+++ precache-manifest.08aad95ffbb0499d72f19e556d0b0aeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf7f46626056592ba8b98dab2197ef1",
+    "url": "/cryptopay/index.html"
+  },
+  {
+    "revision": "4cf1a152fefafefc8fdc",
+    "url": "/cryptopay/static/css/main.2a4f241d.chunk.css"
+  },
+  {
+    "revision": "641a20ee2404f63750a9",
+    "url": "/cryptopay/static/js/2.77189c15.chunk.js"
+  },
+  {
+    "revision": "e0aeb2e98cdd84f2adce32d4a84e83da",
+    "url": "/cryptopay/static/js/2.77189c15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cf1a152fefafefc8fdc",
+    "url": "/cryptopay/static/js/main.9c1a112e.chunk.js"
+  },
+  {
+    "revision": "b8db20a67d6929b91253",
+    "url": "/cryptopay/static/js/runtime-main.23e748dc.js"
+  }
+]);
