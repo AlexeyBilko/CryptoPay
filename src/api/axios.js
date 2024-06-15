21b1e3d86f5b0857e40cb540d91748fd40fa3ae5
Alexey Bilko <bilkoalexey67@gmail.com>
2024-06-15 18:07:52 +0300

    changes axios url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 
-const API_URL = 'http://localhost:5000/api';
-//const API_URL = 'https://cryptopaymentgatewaybackend.azurewebsites.net/api';
+//const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://cryptopaymentgatewaybackend.azurewebsites.net/api';
 
 
 const axiosInstance = axios.create({
